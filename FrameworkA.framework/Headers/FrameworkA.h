a0a21fd03581a134cecf3d0150dbac5bf015037f
Waqas_Dev <waqas.khan@nextgeni.net>
2021-02-06 01:23:33 +0500

    framework added

---
+++ FrameworkA.framework/Headers/FrameworkA.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkA.h
+//  FrameworkA
+//
+//  Created by Waqas Khan on 05/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkA.
+FOUNDATION_EXPORT double FrameworkAVersionNumber;
+
+//! Project version string for FrameworkA.
+FOUNDATION_EXPORT const unsigned char FrameworkAVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkA/PublicHeader.h>
+
+
